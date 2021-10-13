0deb51442285420dbe1b0d652fb0334ccc8ff6a5
Joris Monleon <monleon.joris@gmail.com>
2021-10-13 17:00:25 +0200

    Add react router switch

--- react-cours/src/index.jsx
+++ react-cours/src/index.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
 import Header from './components/Header';
+import Error from './components/Error';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
 
-      <Route exact path="/">
-        <Home />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route path="/survey">
-        <Survey />
-      </Route>
+        <Route path="/survey">
+          <Survey />
+        </Route>
+
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
